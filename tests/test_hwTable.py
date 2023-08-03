c9c95976f6c451630009c0b673e345490a9b95f2
will@hwmedia.com <will@hwmedia.com>
2023-08-03 12:46:26 -0500

    Added Pandas test.

--- tests/test_hwTable.py
+++ tests/test_hwTable.py
@@ -12,6 +12,14 @@ class DtypesTests(unittest.TestCase):
                                 'Percent':('percent', 2),
                                 'Float':('decimal', 3)}), 
                         open('tests/example.html', 'r').read())
+        
+    def test_from_pandas(self):
+        self.assertEqual(hwTable(pd.read_excel('tests/Example.xlsx')).dataHtml(datatypes=
+                                {'Money':('money'),
+                                'Volume':('integer'),
+                                'Percent':('percent', 2),
+                                'Float':('decimal', 3)}), 
+                        open('tests/example.html', 'r').read())
 
 if __name__ == '__main__':
     unittest.main()
